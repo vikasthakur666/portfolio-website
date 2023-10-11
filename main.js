4f78e2b103cf68879a7b4880ca046fdd37d74c03
yash-shakya <136307794+vikasthakur666@users.noreply.github.com>
2023-10-11 09:01:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text",{
+    strings:["Digital Marketer" , "Social Media Marketer" , "Website Designer"],
+    typeSpeed:50,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
